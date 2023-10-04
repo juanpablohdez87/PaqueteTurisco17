962c655e2bdf66a176b4b0186d15d8d3e3a45c9f
Luciano-A1 <141652211+Luciano-A1@users.noreply.github.com>
2023-10-04 12:30:52 -0300

    probar pasaje

--- src/Datos/PasajeDatos.java
+++ src/Datos/PasajeDatos.java
@@ -4,18 +4,37 @@ import Entidades.Pasaje;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
+import javax.swing.JOptionPane;
 
 public class PasajeDatos {
 
-    private Connection con;
-
-    private PreparedStatement ps;
-
-    private ResultSet rs;
+    private static Connection con = Conexion1.getConexion();
+    private static PreparedStatement ps = null;
+    private static ResultSet rs = null;
 
     public static void ciudadPasaje(Pasaje pas) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        String sqlCrear="insert into pasaje(tipoTransporte, idCiuOrigen,importe,estado)values(?,?,?,?)";
+        try {
+            ps=con.prepareStatement(sqlCrear,Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1,pas.getTipoTransporte());
+            ps.setInt(2, pas.getNomCiuOrigen().getIdCiudad());
+            ps.setDouble(3, pas.getImporte());
+            ps.setBoolean(4, pas.isEstado());
+            ps.executeUpdate();
+            rs=ps.getGeneratedKeys();
+            if(rs.next()){
+                pas.setIdPasaje(rs.getInt(1));
+                System.out.println("bien");
+            }else{
+                System.out.println("mal");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al ingresar los datos en la tabla de Pasaje");
+        }
     }
 
     public static void eliminarPasaje(Pasaje pas) {
--- src/paqueteturisco17/Main.java
+++ src/paqueteturisco17/Main.java
@@ -1,7 +1,9 @@
 package paqueteturisco17;
 
 import Datos.CiudadDatos;
+import Datos.PasajeDatos;
 import Entidades.Ciudad;
+import Entidades.Pasaje;
 import java.util.List;
 
 /**
@@ -15,6 +17,8 @@ public class Main {
 //        Ciudad ab = new Ciudad(2, "Guiñazu", "Republica de Cordoba", true, "Cordoba de Anda Lucia");
         Ciudad m = CiudadDatos.buscarCiudad("General Belgrano");
         System.out.println("id = " + m.getIdCiudad());
+        Pasaje b = new Pasaje("Colect", 2300.52, m, true);
+        PasajeDatos.ciudadPasaje(b);
 //        CiudadDatos.modificarCiudad(ab);
 //         List<Ciudad> lista = CiudadDatos.listarCiu();
 //        CiudadDatos.ciudadGuardar(ab);
